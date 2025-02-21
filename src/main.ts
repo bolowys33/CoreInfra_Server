d6361cf359f1e15db97feef7da225fd68d953e20
Olubodun <bolowys33@gmail.com>
2025-02-21 14:52:36 +0100

    Add specified port

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AppFilter } from './filters/app.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -27,6 +31,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  const PORT = configService.get<number>('PORT') || 3000;
+
+  await app.listen(PORT);
+  console.log(`🚀 Server running on http://localhost:${PORT}/api`);
 }
 bootstrap();
